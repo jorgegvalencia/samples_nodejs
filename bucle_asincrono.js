47b0943b1b87bca96b290dfa6bc33d46c076a3be
Jorge González <jorge.gonzalez@babel.es>
2016-03-07 13:58:54 +0100

    Bucle asíncrono con async.

--- bucle_asincrono.js
+++ bucle_asincrono.js
@@ -2,6 +2,8 @@
 
 console.log('Empiezo');
 
+var async = require('async');
+
 var escribeTras2Segundos = function(texto, callback) {
     setTimeout(function() {
         console.log(texto);
@@ -9,13 +11,14 @@ var escribeTras2Segundos = function(texto, callback) {
     }, 500);
 };
 
-function serie(arr, fn) {
-    if (arr.length == 0) {
-        console.log('fin');
-        return;
+async.eachSeries([1, 2, 3, 4, 5],
+    function(item, next) {
+        escribeTras2Segundos(item, function() {
+        	var res = item % 2 ? null : 'error';
+            next(res); // si parámetro != null => error
+        })
+    },
+    function fin(err) {
+        console.log('fin',err);
     }
-    fn(arr.shift(), function() {
-        serie(arr, fn);
-    });
-}
-serie([1, 2, 3, 4, 5], escribeTras2Segundos);
+);
