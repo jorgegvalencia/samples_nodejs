2ea2331d0d3258ec763b2ed30c7c809a39bb8dba
Jorge González <jorge.gonzalez@babel.es>
2016-03-10 11:03:20 +0100

    Conexión con mongoose y query find simple.

--- app_express/prueba/models/users_model.js
+++ app_express/prueba/models/users_model.js
@@ -1,20 +1,28 @@
 'use strict';
 
-var mongomanager = require('../lib/mongooseManager');
+var conn = require('../lib/mongooseManager');
+var mongoose = require('mongoose');
+
+var userSchema = mongoose.Schema({
+    name: String,
+    age: Number
+});
+
+mongoose.model('usuarios', userSchema); // el nombre del modelo debe coincidir con la colección de la base de datos
 
 // metodos del modelo
 var usersList = {
-	getUsers: function (cb) {
-		// leer datos
-		mongomanager.db.collection('usuarios').find({}).toArray(function (err, usuariosLeidos) {
-			if(err){
-				return cb(err);
-			}
-			cb(null, usuariosLeidos);
-			console.dir(usuariosLeidos);
-			// return
-		});
-	} 
+    getUsers: function(cb) {
+        var User = mongoose.model('usuarios');
+        User.find({}, function(err, datos) {
+            if (err) {
+                cb(err);
+                return;
+            }
+            cb(null, datos);
+            // return;
+        })
+    }
 }
 
 module.exports = usersList;
