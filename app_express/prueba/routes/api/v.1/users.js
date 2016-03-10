25dce7b35eb704000d13305958637b7e72502eee
Jorge González <jorge.gonzalez@babel.es>
2016-03-10 13:55:54 +0100

    Api para operaciones de bbdd.

---
+++ app_express/prueba/routes/api/v.1/users.js
@@ -0,0 +1,46 @@
+'use strict';
+
+var express = require('express');
+var router = express.Router();
+var mongoose = require('mongoose');
+
+var User = mongoose.model('usuarios');
+
+/* GET users listing. */
+router.get('/', function(req, res) {
+
+    User.list(function(err, rows) {
+        if (err) {
+            res.json({ result: false, err: err });
+            return;
+        }
+        res.json({ result: true, rows: rows });
+    })
+});
+
+router.post('/', function(req, res) {
+    var usuario = new User(req.body);
+    usuario.save(function(err, created) {
+        if (err) {
+            res.json({ result: false, err: err });
+            return;
+        }
+        res.json({ result: true, row: created });
+        console.log('Usuario ' + created.name + ' de edad ' + created.age + ' creado.');
+    });
+})
+
+router.put('/:id', function(req, res) {
+    User.update({ _id: req.params.id}, { $set: req.body }, {}, function (err, updated) {
+    	// { multi: true }
+    	if (err) {
+            res.json({ result: false, err: err });
+            return;
+        }
+        res.json({ result: true, row: updated });
+        console.log('Usuario ' + updated.name + ' de edad ' + updated.age + ' actualizado.');
+    })
+})
+
+
+module.exports = router;
